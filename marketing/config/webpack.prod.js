e10c2c17d5e1c4024638d26589d7ab9208898517
raihanmuhammad <michael.raihan@stockbit.com>
2023-02-15 16:54:01 +0700

    fix: typo in webpack prod config of marketing app

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPaths: "/marketing/latest/",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
