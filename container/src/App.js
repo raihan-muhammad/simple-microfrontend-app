879cdc74586ca5f13baf5f051e95c9c64873c009
raihanmuhammad <michael.raihan@stockbit.com>
2023-02-15 16:38:27 +0700

    ci: change & to $ in create invalidation on build step

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
 	return (
     <>
-      <h1>Hi, This is Container!!</h1>
+      <h1>Hi, This is Container!</h1>
       <MarketingApp />
     </>
   );
