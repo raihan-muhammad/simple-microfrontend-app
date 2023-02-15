1f35926ab763b6c0f519d0a95f384cd1c6834cf1
raihanmuhammad <michael.raihan@stockbit.com>
2023-02-15 11:36:32 +0700

    config: add webpack prod and pipeline

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,25 +1,24 @@
 const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 const packageJson = require("./../package.json");
 
-const devConfig = {
-  mode: "development",
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: "index.html",
-    },
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
