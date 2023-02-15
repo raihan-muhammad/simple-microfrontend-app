217b7360430c7a8d9cd3569032960a71eedceaa2
raihanmuhammad <michael.raihan@stockbit.com>
2023-02-15 16:35:55 +0700

    ci: fix typo in create invalidation on build step

--- container/src/App.js
+++ container/src/App.js
@@ -3,11 +3,11 @@ import MarketingApp from './components/MarketingApp';
 
 const App = () => {
 	return (
-		<>
-			<h1>Hi, This is Container!</h1>
-			<MarketingApp/>
-		</>
-	)
+    <>
+      <h1>Hi, This is Container!!</h1>
+      <MarketingApp />
+    </>
+  );
 }
 
 export default App;
