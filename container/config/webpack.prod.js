81d39d6278777ad6534fee18c92b754b2ee71f32
raihanmuhammad <michael.raihan@stockbit.com>
2023-02-16 15:24:07 +0700

    fix: url auth remote webpack prod config

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
       name: "container",
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth@${domain}/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
